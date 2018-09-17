1e0b2c48183c15f1a01ba5fc79a94d5ecf87b34e
DiegoAlosilla <43312463+DiegoAlosilla@users.noreply.github.com>
2018-09-17 18:09:01 -0500

    Add files via upload

--- tambo-desktop/CapaPresentacion/FormNuevoProveedor.cs
+++ tambo-desktop/CapaPresentacion/FormNuevoProveedor.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CapaDatos;
+using CapaEntidades;
+using CapaNegocios;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,35 @@ namespace Principal
 {
     public partial class FormNuevoProveedor : Form
     {
+        private ProveedorNE proveedorNE;
         public FormNuevoProveedor()
         {
             InitializeComponent();
+
+            proveedorNE = new ProveedorNE();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Proveedor proveedor = new Proveedor();
+                ProveedorDAO proveedorDAO = new ProveedorDAO();
+
+                proveedor.Nombre = textBoxNombre.Text;
+                proveedor.Descripcion = textBoxDescripcion.Text;
+                proveedor.Telefono = textBoxTelefono.Text;
+                proveedor.Direccion = textBoxDireccion.Text;
+                proveedor.Email = textBoxCorreo.Text;
+
+                int i = proveedorNE.RegistrarProveedor(proveedor);
+             
+                MessageBox.Show("Registro OK ");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrio un error " + ex.Message);
+            }
         }
     }
 }
