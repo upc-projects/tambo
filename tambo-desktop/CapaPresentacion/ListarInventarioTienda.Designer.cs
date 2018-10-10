fff517f4cb89542509521bee0e0d2c7841c158e4
RodrigoAGM <rod-gm@hotmail.com>
2018-10-10 18:12:05 -0500

    List inventory by store

--- tambo-desktop/CapaPresentacion/ListarInventarioTienda.Designer.cs
+++ tambo-desktop/CapaPresentacion/ListarInventarioTienda.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.cboTienda = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DGInventario = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DGInventario)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(707, 13);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(185, 29);
+            this.btnCargar.TabIndex = 0;
+            this.btnCargar.Text = "Obtener Inventario";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // cboTienda
+            // 
+            this.cboTienda.FormattingEnabled = true;
+            this.cboTienda.Location = new System.Drawing.Point(132, 16);
+            this.cboTienda.Name = "cboTienda";
+            this.cboTienda.Size = new System.Drawing.Size(527, 24);
+            this.cboTienda.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tienda: ";
+            // 
+            // DGInventario
+            // 
+            this.DGInventario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGInventario.Location = new System.Drawing.Point(13, 55);
+            this.DGInventario.Name = "DGInventario";
+            this.DGInventario.RowTemplate.Height = 24;
+            this.DGInventario.Size = new System.Drawing.Size(928, 497);
+            this.DGInventario.TabIndex = 3;
+            // 
+            // ListarInventarioTienda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(953, 564);
+            this.Controls.Add(this.DGInventario);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboTienda);
+            this.Controls.Add(this.btnCargar);
+            this.Name = "ListarInventarioTienda";
             this.Text = "ListarInventarioTienda";
+            this.Load += new System.EventHandler(this.ListarInventarioTienda_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGInventario)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.ComboBox cboTienda;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView DGInventario;
     }
 }
--- tambo-desktop/CapaPresentacion/ListarInventarioTienda.cs
+++ tambo-desktop/CapaPresentacion/ListarInventarioTienda.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaNegocios;
 
 namespace CapaPresentacion
 {
     public partial class ListarInventarioTienda : Form
     {
+        private TiendaNE tienda;
         public ListarInventarioTienda()
         {
             InitializeComponent();
+            tienda = new TiendaNE();
+        }
+
+        private void ListarInventarioTienda_Load(object sender, EventArgs e)
+        {
+            cboTienda.DataSource = tienda.ListarTienda();
+            cboTienda.DisplayMember = "nombre";
+            cboTienda.ValueMember = "id";
+        }
+
+        private void LlenarInvetario()
+        {
+            
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
