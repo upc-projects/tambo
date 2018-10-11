bfb6c5e1490beefbe313d4e643c9dddfe6cf682c
DiegoAlosilla <43312463+DiegoAlosilla@users.noreply.github.com>
2018-10-11 00:26:39 -0500

    Add files via upload

---
+++ tambo-desktop/CapaNegocios/CategoriaNE.cs
@@ -0,0 +1,31 @@
+﻿using CapaDatos;
+using CapaEntidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocios
+{
+    public class CategoriaNE
+    {
+        private CategoriaDAO categoriaDAO;
+
+        public CategoriaNE()
+        {
+            categoriaDAO = new CategoriaDAO();
+        }
+
+        public DataTable listarCategorias()
+        {
+            return categoriaDAO.ListarCategoria();
+        }
+
+        public int RegistrarCategoria(Categoria categoria)
+        {
+            return categoriaDAO.RegistrarProducto(categoria);
+        }
+    }
+}
