4527eb21f0234a9948a4744ee7d11327bb15d3d7
Diego Alosilla <diegoalosilla@gmail.com>
2018-11-15 17:14:29 -0500

    .

---
+++ web-mvc/Tambo/BusinessLayer/ServiceImpl/InventarioServiceImpl.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Service;
+using DataLayer;
+using DataLayer.Repository;
+using DataLayer.RepositoryImpl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ServiceImpl
+{
+    class InventarioServiceImpl : IInventarioService
+    {
+        private IInventarioRespository inventarioRepository;
+
+        public InventarioServiceImpl()
+        {
+            inventarioRepository = new InventarioRepositoryImpl();
+        }
+
+        public bool Delete(Inventario t)
+        {
+            return inventarioRepository.Delete(t);
+        }
+
+        public List<Inventario> FindAll()
+        {
+            return inventarioRepository.FindAll();
+        }
+
+        public Inventario FindById(int? id)
+        {
+            return inventarioRepository.FindById(id);
+        }
+
+        public bool Save(Inventario t)
+        {
+            return inventarioRepository.Save(t);
+        }
+
+        public bool Update(Inventario t)
+        {
+            return inventarioRepository.Update(t);
+        }
+    }
+}
---
+++ web-mvc/Tambo/BusinessLayer/ServiceImpl/ProductoInventarioServiceImpl.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Service;
+using DataLayer;
+using DataLayer.Repository;
+using DataLayer.RepositoryImpl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ServiceImpl
+{
+    public class ProductoInventarioServiceImpl : IProductoInventarioService
+    {
+        private IProductoInventarioRespository productoInventarioRespository;
+
+        public ProductoInventarioServiceImpl()
+        {
+            productoInventarioRespository = new ProductoInventarioRepositoryImpl();
+        }
+
+        public bool Delete(Producto_Inventario t)
+        {
+            return productoInventarioRespository.Delete(t);
+        }
+
+        public List<Producto_Inventario> FindAll()
+        {
+            return productoInventarioRespository.FindAll();
+        }
+
+        public Producto_Inventario FindById(int? id)
+        {
+            return productoInventarioRespository.FindById(id);
+        }
+
+        public bool Save(Producto_Inventario t)
+        {
+            return productoInventarioRespository.Save(t);
+        }
+
+        public bool Update(Producto_Inventario t)
+        {
+            return productoInventarioRespository.Update(t);
+        }
+    }
+}
