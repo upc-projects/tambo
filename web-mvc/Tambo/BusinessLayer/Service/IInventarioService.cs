c3f6993acf97f945735c2eb3dcfad4a5ad87938f
Diego Alosilla <diegoalosilla@gmail.com>
2018-11-15 19:28:02 -0500

    .

--- web-mvc/Tambo/BusinessLayer/Service/IInventarioService.cs
+++ web-mvc/Tambo/BusinessLayer/Service/IInventarioService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Service
 {
-    public interface IInventarioService
+    public interface IInventarioService: CrudService<Inventario>
     {
     }
 }
