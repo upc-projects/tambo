b4ae2d9308dc465239c60a28f35d7d19c352020f
RodrigoAGM <rod-gm@hotmail.com>
2018-11-18 19:47:53 -0500

    Inventario controller added

---
+++ web-mvc/Tambo/Tambo/Controllers/InventariosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Entities_Layer;
+
+namespace Tambo.Controllers
+{
+    public class InventariosController : Controller
+    {
+        private TamboContext db = new TamboContext();
+
+        // GET: Inventarios
+        public ActionResult Index()
+        {
+            var inventario = db.Inventario.Include(i => i.Productos).Include(i => i.Tiendas);
+            return View(inventario.ToList());
+        }
+
+        // GET: Inventarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventario inventario = db.Inventario.Find(id);
+            if (inventario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventario);
+        }
+
+        // GET: Inventarios/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_producto = new SelectList(db.Productos, "id", "nombre");
+            ViewBag.id_tienda = new SelectList(db.Tiendas, "id", "nombre");
+            return View();
+        }
+
+        // POST: Inventarios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,id_tienda,id_producto,stock")] Inventario inventario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Inventario.Add(inventario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_producto = new SelectList(db.Productos, "id", "nombre", inventario.id_producto);
+            ViewBag.id_tienda = new SelectList(db.Tiendas, "id", "nombre", inventario.id_tienda);
+            return View(inventario);
+        }
+
+        // GET: Inventarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventario inventario = db.Inventario.Find(id);
+            if (inventario == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_producto = new SelectList(db.Productos, "id", "nombre", inventario.id_producto);
+            ViewBag.id_tienda = new SelectList(db.Tiendas, "id", "nombre", inventario.id_tienda);
+            return View(inventario);
+        }
+
+        // POST: Inventarios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,id_tienda,id_producto,stock")] Inventario inventario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(inventario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_producto = new SelectList(db.Productos, "id", "nombre", inventario.id_producto);
+            ViewBag.id_tienda = new SelectList(db.Tiendas, "id", "nombre", inventario.id_tienda);
+            return View(inventario);
+        }
+
+        // GET: Inventarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventario inventario = db.Inventario.Find(id);
+            if (inventario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventario);
+        }
+
+        // POST: Inventarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Inventario inventario = db.Inventario.Find(id);
+            db.Inventario.Remove(inventario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
